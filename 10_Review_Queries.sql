74cd50dbf4506e58c7a5f47392cf679f9969cc04
Furkan Yavuz <122623545+Javoose@users.noreply.github.com>
2026-08-26 23:39:44 +0300

    Fix SQL join syntax for clarity

--- 10_Review_Queries.sql
+++ 10_Review_Queries.sql
@@ -16,7 +16,7 @@ SELECT TOP 3
 	B.PageCount
 FROM Books AS B
 INNER JOIN Authors AS A
-ON B.AuthorID = A.AuthorID
+	ON B.AuthorID = A.AuthorID
 ORDER BY PageCount DESC;
 
 -- 3. List all books in the Classic category with their authors' full names
@@ -26,9 +26,9 @@ SELECT
 	A.FirstName + ' ' + A.LastName AS [Author Name]
 FROM Books AS B
 INNER JOIN Categories AS Cat
-ON B.CategoryID = Cat.CategoryID
+	ON B.CategoryID = Cat.CategoryID
 INNER JOIN Authors AS A
-ON B.AuthorID = A.AuthorID
+	ON B.AuthorID = A.AuthorID
 WHERE Cat.CategoryName = 'Classic';
 
 -- 4. Find all customers who currently have at least one book that has not been returned
@@ -57,7 +57,7 @@ SELECT
 	COUNT(B.AuthorID) AS [Number of Books]
 FROM Authors AS A
 INNER JOIN Books AS B
-ON A.AuthorID = B.AuthorID
+	ON A.AuthorID = B.AuthorID
 GROUP BY A.FirstName + ' ' + A.LastName, A.AuthorID;
 
 -- 8. Find authors whose books have an average page count greater than 300
@@ -66,7 +66,7 @@ SELECT
 	AVG(B.PageCount) AS [Average Page Count]
 FROM Authors AS A
 INNER JOIN Books AS B
-ON A.AuthorID = B.AuthorID
+	ON A.AuthorID = B.AuthorID
 GROUP BY A.FirstName + ' ' + A.LastName, A.AuthorID
 HAVING AVG(B.PageCount) > 300;
 
@@ -76,14 +76,14 @@ SELECT
 	COUNT(L.CustomerID) AS [Number of Loans]
 FROM Customers AS Cus
 LEFT JOIN Loans AS L
-ON Cus.CustomerID = L.CustomerID
+	ON Cus.CustomerID = L.CustomerID
 GROUP BY Cus.FirstName + ' ' + Cus.LastName, Cus.CustomerID;
 
 -- 10. Find the three most frequently borrowed books
 SELECT TOP 3 B.Title AS Book, COUNT(L.BookID) AS [Number of Loans]
 FROM Books AS B
 INNER JOIN Loans AS L
-ON B.BookID = L.BookID
+	ON B.BookID = L.BookID
 GROUP BY B.Title, B.BookID
 ORDER BY COUNT(L.BookID) DESC;
 
@@ -91,7 +91,7 @@ ORDER BY COUNT(L.BookID) DESC;
 SELECT B.Title AS [Never Been Borrowed]
 FROM Books AS B
 LEFT JOIN Loans AS L
-ON B.BookID = L.BookID
+	ON B.BookID = L.BookID
 WHERE L.BookID IS NULL;
 
 -- 12. Find customers who have returned at least one book late
@@ -106,7 +106,7 @@ WHERE CustomerID IN
 SELECT Cat.CategoryName AS Category, AVG(B.PageCount) AS [Average Page Count]
 FROM Categories AS Cat
 INNER JOIN Books AS B
-ON Cat.CategoryID = B.CategoryID
+	ON Cat.CategoryID = B.CategoryID
 GROUP BY Cat.CategoryName
 ORDER BY [Average Page Count] DESC;
 
@@ -116,7 +116,7 @@ SELECT TOP 1
 	SUM(B.StockQuantity) AS [Total Stock Quantity of Their Books]
 FROM Authors AS A
 INNER JOIN Books AS B
-ON A.AuthorID = B.AuthorID
+	ON A.AuthorID = B.AuthorID
 GROUP BY A.AuthorID, A.FirstName + ' ' + A.LastName, A.FirstName
 ORDER BY [Total Stock Quantity of Their Books] DESC, A.FirstName ASC;
 
@@ -124,10 +124,10 @@ ORDER BY [Total Stock Quantity of Their Books] DESC, A.FirstName ASC;
 SELECT Cus.FirstName + ' ' + Cus.LastName AS [Customers Who Like Different Categories]
 FROM Customers AS Cus
 INNER JOIN Loans AS L
-ON Cus.CustomerID = L.CustomerID
+	ON Cus.CustomerID = L.CustomerID
 INNER JOIN Books AS B
-ON B.BookID = L.BookID
+	ON B.BookID = L.BookID
 INNER JOIN Categories AS Cat
-ON Cat.CategoryID = B.CategoryID
+	ON Cat.CategoryID = B.CategoryID
 GROUP BY Cus.CustomerID, Cus.FirstName + ' ' + Cus.LastName
 HAVING COUNT(DISTINCT Cat.CategoryID) > 1;
